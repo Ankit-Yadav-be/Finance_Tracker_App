1dd9b2a7c0cd2e39c56f3b187c587a7e3acb1ce3
Ankit yadav <ay870421@gmail.com>
2025-02-17 09:36:12 +0530

    production ready app

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,13 @@ import dbconnection from "./config/db.js"
 import transactionRoutes from "./routes/TrasactionApi.js"
 import categoryRoutes from "./routes/CategoryApi.js"
 import budgetRoutes from "./routes/BudgetApi.js"
-
+import path from "path";
 dotenv.config();
 const app = express();
 dbconnection();
+
+const _dirname = path.resolve();
+
 
 app.use(express.json());
 app.use(cors());
@@ -17,6 +20,10 @@ app.use("/api/transactions", transactionRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/budgets", budgetRoutes);
 
+app.use(express.static(path.join(_dirname, "/frontends/dist")))
+app.get('*',(_,res)=>{
+  res.sendFile(path.resolve(_dirname, "frontends", "dist", "index.html"));
+})
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
